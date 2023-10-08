83d2f40f2ebf9c063f3c7f270145a287deda0a47
largomst <dingrui.zhen@gmail.com>
2023-10-08 17:35:30 +0800

    feat: finish scanner

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ TokenType = Enum(
     # Single-character tokens.
     LEFT_PAREN RIGHT_PAREN LEFT_BRACE RIGHT_BRACE COMMA DOT MINUS PLUS SEMICOLON SLASH STAR
     # One or two character tokens.
-    BANG BANG_EQUAL EQUAL EQUAL_EQUAL GREATER GREATER_EQUAL
+    BANG BANG_EQUAL EQUAL EQUAL_EQUAL GREATER GREATER_EQUAL LESS LESS_EQUAL
     # Literals.
     IDENTIFIER STRING NUMBER
     # Keywords.
@@ -34,11 +34,30 @@ class Token:
 class Scanner:
     def __init__(self, source: str):
         self.source = source
-        self.tokens = []
 
+        self.tokens = []
         self.start = 0
         self.current = 0
         self.line = 1
+
+        self.keywords = {
+            'and': TokenType.AND,
+            'class': TokenType.CLASS,
+            'else': TokenType.ELSE,
+            'false': TokenType.FALSE,
+            'fun': TokenType.FUN,
+            'for': TokenType.FOR,
+            'if': TokenType.IF,
+            'nil': TokenType.NIL,
+            'or': TokenType.OR,
+            'print': TokenType.PRINT,
+            'return': TokenType.RETURN,
+            'super': TokenType.SUPER,
+            'this': TokenType.THIS,
+            'true': TokenType.TRUE,
+            'var': TokenType.VAR,
+            'while': TokenType.WHILE,
+        }
 
     def scan_tokens(self):
         while not self.is_at_end():
@@ -50,6 +69,125 @@ class Scanner:
 
     def is_at_end(self):
         return self.current >= len(self.source)
+
+    def scan_token(self):
+        c = self.advance()
+        match c:
+            case '(':
+                self.add_token(TokenType.LEFT_PAREN)
+            case ')':
+                self.add_token(TokenType.RIGHT_PAREN)
+            case '{':
+                self.add_token(TokenType.LEFT_BRACE)
+            case '}':
+                self.add_token(TokenType.RIGHT_BRACE)
+            case ',':
+                self.add_token(TokenType.COMMA)
+            case '.':
+                self.add_token(TokenType.DOT)
+            case '-':
+                self.add_token(TokenType.MINUS)
+            case '+':
+                self.add_token(TokenType.PLUS)
+            case ';':
+                self.add_token(TokenType.SEMICOLON)
+            case '*':
+                self.add_token(TokenType.STAR)
+            case '!':
+                self.add_token(TokenType.BANG_EQUAL if self.match('=') else TokenType.BANG)
+            case '=':
+                self.add_token(TokenType.EQUAL_EQUAL if self.match('=') else TokenType.EQUAL)
+            case '<':
+                self.add_token(TokenType.LESS_EQUAL if self.match('=') else TokenType.LESS)
+            case '>':
+                self.add_token(TokenType.GREATER_EQUAL if self.match('=') else TokenType.GREATER)
+            case '/':
+                if self.match('/'):
+                    while self.peek() != '\n' and self.is_at_end():
+                        self.advance()
+                else:
+                    self.add_token(TokenType.SLASH)
+            case ' ' | '\r' | '\t':
+                pass
+            case '\n':
+                self.line += 1
+            case '"':
+                self.string()
+            case _:
+                if self.is_digit(c):
+                    self.number()
+                elif self.is_alpha(c):
+                    self.identifier()
+                else:
+                    error(self.line, 'Unexpected character.')
+
+    def identifier(self):
+        while self.is_alhpa_numberic(self.peek()):
+            self.advance()
+        text = self.source[self.start : self.current]
+        type_ = self.keywords.get(text, TokenType.IDENTIFIER)
+        self.add_token(type_)
+
+    def is_alpha(self, c: str) -> bool:
+        return 'a' <= c <= 'z' or 'A' <= c <= 'Z' or c == '_'
+
+    def is_alhpa_numberic(self, c: str) -> bool:
+        return self.is_alpha(c) or self.is_digit(c)
+
+    def is_digit(self, c: str) -> bool:
+        return '0' <= c <= '9'
+
+    def number(self):
+        while self.is_digit(self.peek()):
+            self.advance()
+
+        if self.peek() == '.' and self.is_digit(self.peek_next()):
+            self.advance()
+
+        while self.is_digit(self.peek()):
+            self.advance()
+
+        self.add_token(Token.NUMBER, float(self.source[self.current : self.current]))
+
+    def peek_next(self):
+        if self.current + 1 > len(self.source):
+            return '\0'
+        return self.source[self.current + 1]
+
+    def string(self):
+        while self.peek() != '"' and not self.is_at_end():
+            if self.peek() == '\n':
+                self.line += 1
+            self.advance()
+        if self.is_at_end():
+            error(self.line, 'Unterminated string.')
+            return
+        self.advance()
+
+        value = self.source[self.start + 1 : self.current - 1]
+        self.add_token(TokenType.STRING, value)
+
+    def peek(self):
+        return '\0' if self.is_at_end() else self.source[self.current]
+
+    def match(self, expected: str) -> bool:
+        # match 是有条件的 advance
+        if self.is_at_end():
+            return False
+        elif self.source[self.current] != expected:
+            return False
+        else:
+            self.current += 1
+            return True
+
+    def advance(self):
+        c = self.source[self.current]
+        self.current += 1
+        return c
+
+    def add_token(self, token_type: TokenType, literal: object = None):
+        text = self.source[self.start : self.current]
+        self.tokens.append(Token(token_type, text, literal, self.line))
 
 
 def run_file(path: str):
