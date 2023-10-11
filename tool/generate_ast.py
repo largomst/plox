a03c47d50b394c1bc16d14e714fb90baecf7f6c0
largomst <dingrui.zhen@gmail.com>
2023-10-11 14:55:53 +0800

    fix: type annotation error

--- tool/generate_ast.py
+++ tool/generate_ast.py
@@ -23,17 +23,18 @@ def define_visitor(f, base_name: str, types: List[str]):
     for type_ in types:
         type_name = type_.split(':')[0].strip()
         f.write(f'    @abstractmethod\n')
-        f.write(f'    def visit{type_name}{base_name}({base_name.lower()}: {type_name}):\n')
+        f.write(f"    def visit{type_name}{base_name}({base_name.lower()}: '{type_name}'):\n")
         f.write(f'        pass\n\n')
 
 
 def define_ast(output_dir: str, base_name: str, types: List[str]):
     path = Path(output_dir) / f'{base_name}.py'
     with open(path, 'w', encoding='utf-8') as f:
-        f.write('from abc import ABC, abstractmethod\n\n\n')
+        f.write('from abc import ABC, abstractmethod\n')
+        f.write('from main import Token\n\n\n')
         f.write(f'class {base_name}(ABC):\n')
         f.write(f'    @abstractmethod\n')
-        f.write(f'    def accept(self, visitor: Visitor):\n        pass\n\n\n')
+        f.write(f"    def accept(self, visitor: 'Visitor'):\n        pass\n\n\n")
         define_visitor(f, base_name, types)
         for type_ in types:
             class_name = type_.split(':')[0].strip()
