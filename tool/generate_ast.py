c776aac62ed61866a139e27a8203d64cd1972a7a
largomst <dingrui.zhen@gmail.com>
2023-10-11 10:27:57 +0800

    feat: add visitor generator

--- tool/generate_ast.py
+++ tool/generate_ast.py
@@ -13,18 +13,33 @@ def define_type(f, base_name: str, class_name: str, field_list: str):
     for field in field_list.split(', '):
         name = field.split(' ')[1]
         f.write(f'        self.{name} = {name}\n')
-    f.write('\n\n')
+    f.write('\n')
+    f.write('    def accept(self, visitor: Visitor):\n')
+    f.write(f'        return visitor.visit{class_name}{base_name}(self)\n\n\n')
+
+
+def define_visitor(f, base_name: str, types: List[str]):
+    f.write('class Visitor(ABC):\n')
+    for type_ in types:
+        type_name = type_.split(':')[0].strip()
+        f.write(f'    @abstractmethod\n')
+        f.write(f'    def visit{type_name}{base_name}({base_name.lower()}: {type_name}):\n')
+        f.write(f'        pass\n\n')
 
 
 def define_ast(output_dir: str, base_name: str, types: List[str]):
     path = Path(output_dir) / f'{base_name}.py'
     with open(path, 'w', encoding='utf-8') as f:
         f.write('from abc import ABC, abstractmethod\n\n\n')
-        f.write(f'class {base_name}(ABC):\n    pass\n\n\n')
+        f.write(f'class {base_name}(ABC):\n')
+        f.write(f'    @abstractmethod\n')
+        f.write(f'    def accept(self, visitor: Visitor):\n        pass\n\n\n')
+        define_visitor(f, base_name, types)
         for type_ in types:
             class_name = type_.split(':')[0].strip()
             fields = type_.split(':')[1].strip()
             define_type(f, base_name, class_name, fields)
+        f.write('\n')
 
 
 def main():
@@ -38,7 +53,7 @@ def main():
         [
             'Binary   : Expr left, Token operator, Expr right',
             'Grouping : Expr expression',
-            'Literal  : Object value',
+            'Literal  : object value',
             'Unary    : Token operator, Expr right',
         ],
     )
